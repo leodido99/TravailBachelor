c10647089d3cf1b3914ea01c8d4381faaecd4516
Léonard Bise - Desktop <leonard.bise@gmail.com>
2018-06-26 14:28:20 +0200

    Setup Zephyr Eclipse project for SODAQ One v3 board

---
+++ src/Zephyr_sodaq_one_v3/build.sh
@@ -0,0 +1,4 @@
+mkdir build
+cd build
+#cmake -GNinja -DBOARD=sodaq_one_v3 -DBOARD_ROOT=/home/leonard/gitrepo/TravailBachelor/src/Zephyr_sodaq_one_v3/boards ..
+cmake -GNinja ..
---
+++ src/Zephyr_sodaq_one_v3/path.sh
@@ -0,0 +1,1 @@
+source ../../../zephyr/zephyr-env.sh 
