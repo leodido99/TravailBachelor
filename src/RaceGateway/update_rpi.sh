83c358d8975110a19691a3946f55b2ecf3ba9fe6
Léonard Bise <leonard.bise@gmail.com>
2018-07-11 20:08:23 +0200

    Fixed path where to load bin

--- src/RaceGateway/update_rpi.sh
+++ src/RaceGateway/update_rpi.sh
@@ -1,4 +1,4 @@
-scp out/race_app_server.out pi@192.168.1.14:race_tracker_gateway/TravailBachelor/src/RaceGateway/src/out
+scp out/race_app_server.out pi@192.168.1.14:race_tracker_gateway/TravailBachelor/src/RaceGateway/out
 
 
 
