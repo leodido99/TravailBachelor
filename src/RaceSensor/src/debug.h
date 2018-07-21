4ecf7f1155b74062d1b6944b9bc0ec884fa55ed9
Léonard Bise <leonard.bise@gmail.com>
2018-07-21 17:38:06 +0200

    Updated drivers

--- src/RaceSensor/src/debug.h
+++ src/RaceSensor/src/debug.h
@@ -10,9 +10,11 @@
 #ifndef SRC_DEBUG_H_
 #define SRC_DEBUG_H_
 
-#define DEBUG 1
+#define DEBUG_RN2483_LORA 1
+#define DEBUG_LSM303AGR 1
+#define DEBUG_UBLOXEVA8M 1
 
 #define DBG_PRINTK(fmt, ...) \
-            do { if (DEBUG) printk(fmt, __VA_ARGS__); } while (0)
+            do { if (DEBUG) printk(fmt, ## __VA_ARGS__); } while (0)
 
 #endif /* SRC_DEBUG_H_ */
--- src/RaceSensor/src/drivers/RN2483_lora.c
+++ src/RaceSensor/src/drivers/RN2483_lora.c
@@ -12,6 +12,8 @@
 
 #include "debug.h"
 
+#define DEBUG DEBUG_RN2483_LORA
+
 #include <string.h>
 #include <stdio.h>
 
@@ -32,6 +34,22 @@
  * Command to transmit radio
  */
 #define RN2483_LORA_RADIO_TX_CMD "radio tx "
+
+/**
+ * Command to set the spreading factor
+ */
+#define RN2483_LORA_RADIO_SET_SF "radio set sf "
+
+/**
+ * Command to pause the MAC layer
+ */
+#define RN2483_LORA_MAC_PAUSE "mac pause"
+
+/**
+ * Command to set the radio output power
+ */
+#define RN2483_LORA_RADIO_SET_PWR "radio set pwr "
+
 /**
  * Size of the command to transmit radio (nb chars)
  */
@@ -55,7 +73,12 @@ static struct device *lora_uart;
 /**
  * Thread priority
  */
-#define RN2483_LORA_PRIORITY 7
+#define RN2483_LORA_PRIORITY 0
+
+/**
+ * Amount of time the thread sleeps after each iterations
+ */
+#define RN2483_LORA_THREAD_SLEEP_MS 100
 
 /**
  *  Buffer size
@@ -63,20 +86,19 @@ static struct device *lora_uart;
 #define BUFF_SIZE 256
 
 /**
- * RX Data Buffer
+ * Number of buffers
+ */
+#define BUFF_NB 4
+
+/**
+ * RX Data index
  */
 u32_t rx_data_buf_idx = 0;
+
+/**
+ * RX Data Buffer used by the irq
+ */
 u8_t rx_data_buf[BUFF_SIZE];
-
-/**
- * RX data is ready
- */
-static volatile bool rx_data_rdy = false;
-
-/**
- * Ready to send (i.e. received system ver at beginning)
- */
-static volatile bool ready_to_send = false;
 
 /**
  * New command is ready to be sent
@@ -84,19 +106,62 @@ static volatile bool ready_to_send = false;
 static volatile bool new_cmd_rdy = false;
 
 /**
- * Command index and buffer
+ * Command index
  */
 u32_t cmd_idx = 0;
+
+/**
+ * Buffer used to store the command to send
+ */
 u8_t cmd_buf[BUFF_SIZE];
 
 /**
- * Wait for buffer
+ * Wait for mechanism structure
  */
-static volatile bool wait_for = false;
-char wait_for_buf[BUFF_SIZE];
+typedef struct {
+	bool wait_for;
+	int wait_for_nb;
+	char wait_for_buf[BUFF_NB][BUFF_SIZE];
+	struct k_sem* wait_for_sem;
+	int wait_for_match_idx;
+} rn2483_lora_wait_for_priv;
+
+/**
+ * Structure used to store the wait for mechanism state
+ */
+static rn2483_lora_wait_for_priv wait_for_priv = {
+	.wait_for = false,
+	.wait_for_nb = 0,
+	.wait_for_sem = NULL
+};
+
+/**
+ * Size of the largest message we can receive from the LoRa module
+ * "RN2483 1.0.3 Mar 22 2017 06:00:42\r\n"
+ */
+#define RN2483_LORA_MSG_SIZE 40
+
+/**
+ * Size of the receive message queue
+ */
+#define RN2483_LORA_RX_MSG_QUEUE_SIZE 2
+
+/**
+ * Structure containing an rx message
+ */
+typedef struct {
+	char msg[RN2483_LORA_MSG_SIZE];
+} rn2483_lora_rx_msg;
+
+/* Define a message queue to hold the rx messages */
+K_MSGQ_DEFINE(rn2483_lora_rx_msgs, sizeof(rn2483_lora_rx_msg), RN2483_LORA_RX_MSG_QUEUE_SIZE, 4);
+
+/* Semaphore used to wait for a certain message */
+K_SEM_DEFINE(sem_lora_wait_for_rx_msg, 0, 1);
 
 static void rn2483_lora_irq_handler(struct device *dev) {
 	int bytes_read;
+	rn2483_lora_rx_msg rx_msg;
 
 	uart_irq_update(dev);
 
@@ -109,45 +174,67 @@ static void rn2483_lora_irq_handler(struct device *dev) {
 		while ((bytes_read = uart_fifo_read(dev, (u8_t *)(&rx_data_buf[rx_data_buf_idx]), sizeof(rx_data_buf) - rx_data_buf_idx))) {
 			/* Check last char */
 			if (rx_data_buf[rx_data_buf_idx + bytes_read - 1] == '\n') {
-				rx_data_rdy = true;
-				/* Discard carriage return */
-				rx_data_buf[rx_data_buf_idx + bytes_read - 1] = '\0';
+				/* Discard \r\n return */
+				rx_data_buf[rx_data_buf_idx + bytes_read - 2] = '\0';
+				/* Message is ready, add to queue */
+				strcpy(rx_msg.msg, rx_data_buf);
+				/* Add message to queue */
+				if (k_msgq_put(&rn2483_lora_rx_msgs, &rx_msg, K_NO_WAIT) < 0) {
+					DBG_PRINTK("%s: Couldn't store rx msg in queue\n", __func__);
+				}
+				rx_data_buf_idx = 0;
+			} else {
+				rx_data_buf_idx += bytes_read;
 			}
-			rx_data_buf_idx += bytes_read;
+		}
+	}
+}
+
+static void check_waiting_for(rn2483_lora_rx_msg* msg) {
+	if (wait_for_priv.wait_for) {
+		if (wait_for_priv.wait_for_nb == 0) {
+			DBG_PRINTK("%s: Received waiting for any message: \"%s\"\n", __func__, msg->msg);
+			/* If we are waiting for a message but the count is 0,
+			 * it means we are waiting for any messages */
+			wait_for_priv.wait_for = false;
+			wait_for_priv.wait_for_match_idx = 0;
+			k_sem_give(wait_for_priv.wait_for_sem);
+			return;
+		}
+
+		/* Check message received is one of the expected one */
+		for(int i = 0; i < wait_for_priv.wait_for_nb; i++) {
+			if (strcmp(msg->msg, wait_for_priv.wait_for_buf[i]) == 0) {
+				DBG_PRINTK("%s: Received waiting for message: \"%s\"\n", __func__, msg->msg);
+				/* Found message */
+				wait_for_priv.wait_for = false;
+				wait_for_priv.wait_for_nb = 0;
+				wait_for_priv.wait_for_match_idx = i;
+				k_sem_give(wait_for_priv.wait_for_sem);
+				break;
+			}
 		}
 	}
 }
 
 void rn2483_lora_thread(void) {
 	char sent;
+	rn2483_lora_rx_msg rx_msg;
+
+	DBG_PRINTK("%s: Enter thread\n", __func__);
+
+	/* When the LoRa module is switched on, it will automatically
+	 * send its version number */
+	if (k_msgq_get(&rn2483_lora_rx_msgs, &rx_msg, K_MSEC(200))) {
+		DBG_PRINTK("%s: Couldn't read rx message from queue\n", __func__);
+	}
+	DBG_PRINTK("%s: %s\n", __func__, rx_msg.msg);
+
+	/* Unblock the init function */
+	k_sem_give(wait_for_priv.wait_for_sem);
 
 	while(1) {
-		if (rx_data_rdy) {
-			if (!ready_to_send) {
-				/* Before being able to send anything, we need
-				 * to wait for the sys version from the RN2483 */
-				ready_to_send = true;
-			}
-
-			if (wait_for) {
-				if (wait_for_buf[0] == '\0') {
-					/* Wait for any reply */
-					wait_for = false;
-				} else {
-					/* Check the reply */
-					if (strcmp(wait_for_buf, rx_data_buf) == 0) {
-						wait_for = false;
-					}
-				}
-			}
-			/* setup for new reading */
-			rx_data_rdy = false;
-			rx_data_buf_idx = 0;
-
-			DBG_PRINTK("%s: RX: %s\n", __func__, rx_data_buf);
-		}
-
-		if (ready_to_send && new_cmd_rdy) {
+		if (new_cmd_rdy) {
 			/* Send command by polling */
 			sent = uart_poll_out(lora_uart, cmd_buf[cmd_idx]);
 			if (sent == '\n') {
@@ -156,83 +243,84 @@ void rn2483_lora_thread(void) {
 			}
 			cmd_idx++;
 		}
+
+		if (k_msgq_num_used_get(&rn2483_lora_rx_msgs) > 0) {
+			/* Data ready */
+			if (k_msgq_get(&rn2483_lora_rx_msgs, &rx_msg, K_FOREVER)) {
+				DBG_PRINTK("%s: Couldn't read rx message from queue\n", __func__);
+			}
+			DBG_PRINTK("%s: RX: %s\n", __func__, rx_msg.msg);
+
+			/* Check if we are waiting on this message */
+			check_waiting_for(&rx_msg);
+		}
+
 		/* TODO uart_err_check */
+		k_sleep(RN2483_LORA_THREAD_SLEEP_MS);
 	}
 }
 
-int rn2483_lora_cmd(char* cmd) {
-	if (!new_cmd_rdy) {
-		if (strlen(cmd) <= (BUFF_SIZE - 2)) {
-			memcpy(cmd_buf, cmd, strlen(cmd));
-			cmd_buf[strlen(cmd)] = '\r';
-			cmd_buf[strlen(cmd) + 1] = '\n';
-			new_cmd_rdy = true;
-			cmd_idx = 0;
-		} else {
-			DBG_PRINTK("%s: Cmd too long %d (max:%d)\n", __func__, strlen(cmd), BUFF_SIZE);
-			return RN2483_LORA_CMD_TOO_LONG;
+int rn2483_lora_wait_for_either_replies(char replies[][RN2483_LORA_MSG_SIZE], unsigned int nb_replies, int* reply_match_idx) {
+
+	if (nb_replies > BUFF_NB) {
+		DBG_PRINTK("%s: Too many replies : %d > %d\n", __func__, nb_replies, BUFF_NB);
+		return RN2483_LORA_TOO_MANY_REPLIES;
+	}
+
+	if (!wait_for_priv.wait_for) {
+		wait_for_priv.wait_for = true;
+		wait_for_priv.wait_for_nb = nb_replies;
+		for(int i = 0; i < nb_replies; i++) {
+			/* Copy all the replies */
+			strcpy(wait_for_priv.wait_for_buf[i], replies[i]);
 		}
+
+		DBG_PRINTK("%s: Waiting for replies %d\n", __func__, nb_replies);
+		/* Take semaphore */
+		k_sem_take(wait_for_priv.wait_for_sem, K_FOREVER);
+		*reply_match_idx = wait_for_priv.wait_for_match_idx;
 	} else {
-		DBG_PRINTK("%s: Module busy\n", __func__);
+		DBG_PRINTK("%s: Already waiting for message(s)\n", __func__);
 		return RN2483_LORA_BUSY;
 	}
+
 	return RN2483_LORA_SUCCESS;
 }
 
-void rn2483_lora_wait_for_any_reply() {
-	if (!wait_for) {
-		wait_for_buf[0] = '\0';
-		wait_for = true;
-		while(wait_for) {
-			k_sleep(200);
-		}
-	}
+int rn2483_lora_wait_for_any_reply() {
+	return rn2483_lora_wait_for_either_replies(NULL, 0, NULL);
 }
 
-void rn2483_lora_wait_for_reply(char* reply) {
-	if (!wait_for && strlen(reply) <= (BUFF_SIZE - 2)) {
-		strcpy(wait_for_buf, reply);
-		wait_for_buf[strlen(wait_for_buf)] = '\r';
-		wait_for_buf[strlen(wait_for_buf)+1] = '\0';
-		wait_for = true;
-		while(wait_for) {
-			k_sleep(200);
-		}
-	}
-}
-
-void rn2483_lora_wait_cmd() {
-	while(new_cmd_rdy) {
-		k_sleep(200);
-	}
+int rn2483_lora_wait_for_reply(char* reply) {
+	return rn2483_lora_wait_for_either_replies((char(*)[RN2483_LORA_MSG_SIZE])reply, 1, NULL);
 }
 
 int rn2483_lora_init(const char *device_name) {
-	rx_data_rdy = false;
 	rx_data_buf_idx = 0;
 	new_cmd_rdy = false;
-	ready_to_send = false;
+
+	wait_for_priv.wait_for_sem = &sem_lora_wait_for_rx_msg;
 
 	/* Reset device */
 	if (rn2483_lora_reset()) {
 		return RN2483_LORA_BINDING_FAILED;
 	}
+
 	/* Bind to LoRa UART */
 	lora_uart = device_get_binding(device_name);
 	if (!lora_uart) {
 		DBG_PRINTK("%s: Couldn't bind LoRa UART device\n", __func__);
 		return RN2483_LORA_BINDING_FAILED;
 	}
+
 	/* Configure interrupt handler */
 	uart_irq_callback_set(lora_uart, rn2483_lora_irq_handler);
 
 	/* Enable interrupts */
 	uart_irq_rx_enable(lora_uart);
 
-	/* Wait for the reading of the system version */
-	while(!ready_to_send) {
-		k_sleep(200);
-	}
+	/* Wait for the RN2483 version message */
+	k_sem_take(wait_for_priv.wait_for_sem, K_FOREVER);
 
 	return RN2483_LORA_SUCCESS;
 }
@@ -260,29 +348,133 @@ int rn2483_lora_reset() {
 	return RN2483_LORA_SUCCESS;
 }
 
-int rn2483_lora_radio_tx(u8_t* data, u32_t size) {
-	char cmd_buf[BUFF_SIZE];
-	char tmp[3];
-	int i;
-
+int rn2483_lora_cmd(char* cmd) {
 	if (!new_cmd_rdy) {
-		if (size <= RN2483_LORA_MAX_PAYLOAD_SIZE) {
-			strcpy(cmd_buf, RN2483_LORA_RADIO_TX_CMD);
-			for(i = 0; i < size; i++) {
-				snprintf(tmp, 3, "%02X", data[i]);
-				strcat(cmd_buf, tmp);
-			}
-			/* Send command */
-			return rn2483_lora_cmd(cmd_buf);
+		if (strlen(cmd) <= (BUFF_SIZE - 3)) {
+			memcpy(cmd_buf, cmd, strlen(cmd));
+			cmd_buf[strlen(cmd)] = '\r';
+			cmd_buf[strlen(cmd) + 1] = '\n';
+			cmd_buf[strlen(cmd) + 2] = '\0';
+			new_cmd_rdy = true;
+			cmd_idx = 0;
 		} else {
-			DBG_PRINTK("%s: Command too long %d (max %d)\n", __func__, size, RN2483_LORA_MAX_PAYLOAD_SIZE);
+			DBG_PRINTK("%s: Cmd too long %d (max:%d)\n", __func__, strlen(cmd), BUFF_SIZE);
 			return RN2483_LORA_CMD_TOO_LONG;
 		}
 	} else {
-		DBG_PRINTK("%s: Module busy\n", __func__);
+		DBG_PRINTK("%s: Command is currently being sent\n", __func__);
 		return RN2483_LORA_BUSY;
 	}
 	return RN2483_LORA_SUCCESS;
 }
 
+int rn2483_lora_radio_tx(u8_t* data, u32_t size) {
+	char cmd_buf[BUFF_SIZE];
+	char tmp[3];
+	char replies[4][RN2483_LORA_MSG_SIZE] = { "busy", "invalid_param", "radio_err", "radio_tx_ok" };
+	int i;
+	int err;
+	int reply_idx = 0;
+
+	if (size <= RN2483_LORA_MAX_PAYLOAD_SIZE) {
+		strcpy(cmd_buf, RN2483_LORA_RADIO_TX_CMD);
+		for(i = 0; i < size; i++) {
+			snprintf(tmp, 3, "%02X", data[i]);
+			strcat(cmd_buf, tmp);
+		}
+
+		/* Send command */
+		err = rn2483_lora_cmd(cmd_buf);
+		if (err) {
+			return err;
+		}
+
+		/* Wait for reply */
+		err = rn2483_lora_wait_for_either_replies(replies, 4, &reply_idx);
+		if (err) {
+			return err;
+		}
+		if (reply_idx != 3) {
+			DBG_PRINTK("%s: Unexpected reply %s\n", __func__, replies[reply_idx]);
+			return RN2483_LORA_ERR_REPLY;
+		}
+	} else {
+		DBG_PRINTK("%s: Command too long %d (max %d)\n", __func__, size, RN2483_LORA_MAX_PAYLOAD_SIZE);
+		return RN2483_LORA_CMD_TOO_LONG;
+	}
+	return RN2483_LORA_SUCCESS;
+}
+
+int rn2483_lora_radio_set_sf(char* sf) {
+	char cmd_buf[BUFF_SIZE];
+	char replies[2][RN2483_LORA_MSG_SIZE] = { "ok", "invalid_param" };
+	int err;
+	int reply_idx = 0;
+
+	/* Build command */
+	strcpy(cmd_buf, RN2483_LORA_RADIO_SET_SF);
+	strcat(cmd_buf, sf);
+
+	/* Send command */
+	err = rn2483_lora_cmd(cmd_buf);
+	if (err) {
+		return err;
+	}
+
+	/* Wait for reply */
+	err = rn2483_lora_wait_for_either_replies(replies, 2, &reply_idx);
+	if (err) {
+		return err;
+	}
+	if (reply_idx != 0) {
+		DBG_PRINTK("%s: Unexpected reply %s\n", __func__, replies[reply_idx]);
+		return RN2483_LORA_ERR_REPLY;
+	}
+	return RN2483_LORA_SUCCESS;
+}
+
+int rn2483_lora_radio_set_pwr(u8_t pwr) {
+	char cmd_buf[BUFF_SIZE];
+	char replies[2][RN2483_LORA_MSG_SIZE] = { "ok", "invalid_param" };
+	int err;
+	int reply_idx = 0;
+
+	snprintf(cmd_buf, BUFF_SIZE, "%s%d", RN2483_LORA_RADIO_SET_PWR, pwr);
+
+	/* Send command */
+	err = rn2483_lora_cmd(cmd_buf);
+	if (err) {
+		return err;
+	}
+
+	/* Wait for reply */
+	err = rn2483_lora_wait_for_either_replies(replies, 2, &reply_idx);
+	if (err) {
+		return err;
+	}
+	if (reply_idx != 0) {
+		DBG_PRINTK("%s: Unexpected reply %s\n", __func__, replies[reply_idx]);
+		return RN2483_LORA_ERR_REPLY;
+	}
+
+	return RN2483_LORA_SUCCESS;
+}
+
+int rn2483_lora_pause_mac() {
+	char cmd_buf[BUFF_SIZE];
+	int err;
+
+	strcpy(cmd_buf, RN2483_LORA_MAC_PAUSE);
+
+	/* Send command */
+	err = rn2483_lora_cmd(cmd_buf);
+	if (err) {
+		return err;
+	}
+
+	/* Wait for reply */
+	return rn2483_lora_wait_for_any_reply();
+}
+
+/* Thread definition for the RN2483 LoRa module */
 K_THREAD_DEFINE(rn2483_lora_thread_id, RN2483_LORA_STACKSIZE, rn2483_lora_thread, NULL, NULL, NULL, RN2483_LORA_PRIORITY, 0, K_NO_WAIT);
