749d9e392e4d9e5267fcf15e8034b49419c45a01
Léonard Bise <leonard.bise@gmail.com>
2018-08-07 21:30:34 +0200

    Updated UML Work on DB connection and queries

--- src/RaceGateway/src/race_app_server/race_tracker_data.cpp
+++ src/RaceGateway/src/race_app_server/race_tracker_data.cpp
@@ -27,16 +27,30 @@ race_tracker_data::~race_tracker_data()
 
 int race_tracker_data::insert_data_point(race_mode_record* data_point)
 {
-	pqxx::connection C(connection_str);
-	if (!C.is_open()) {
+	pqxx::connection c(connection_str);
+	pqxx::work t{c};
+
+	if (!c.is_open()) {
 		throw std::runtime_error("Cannot connect to database " + connection_str);
 	}
 
+	c.prepare("insert_data_point", "INSERT INTO race_tracker.data_point "
+		  "(competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status)"
+		  "VALUES ($1, $2, $3, $4, ST_MakePoint($5, $6), $7, $8, $9, $10, $11);");
 
 	std::string insert_query = "INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status)" \
 			 	   "VALUES (1, 1, 0, '2018-05-26 08:30:00-00', ST_MakePoint(46.9933, 6.91612), 130, 160, 5, 9.9, 0);";
 
 	pqxx::result r = this->get_active_competitions(data_point->get_id());
+	if (r.size() > 1) {
+		throw std::runtime_error("Sensor active on " + r.size() + " competitions");
+	}
+
+	t.prepared("insert_data_point")().exec();
+
+
+
+
 
 	log(logINFO) << "Query results:";
 	for (auto row: r) {
@@ -53,7 +67,7 @@ int race_tracker_data::insert_data_point(race_mode_record* data_point)
 pqxx::result race_tracker_data::get_active_competitions(uint16_t sensor_id)
 {
 	pqxx::connection c(connection_str);
-	pqxx::work txn{c};
+	pqxx::work t{c};
 
 	if (!c.is_open()) {
 		throw std::runtime_error("Cannot connect to database " + connection_str);
@@ -69,17 +83,5 @@ pqxx::result race_tracker_data::get_active_competitions(uint16_t sensor_id)
 			                     "race_tracker.competitor_registration.sensor_id = $1 "
 			                     "AND race_tracker.competition.active = True;");
 
-	return txn.prepared("get_active_competitions")(sensor_id).exec();
-
-	/*pqxx::result r = txn.exec("select race_tracker.competition.competition_id from " \
-				  "race_tracker.competitor_registration INNER JOIN "
-				  "race_tracker.competitor ON "
-				  "(race_tracker.competitor_registration.competitor_id = "
-				  "race_tracker.competitor.competitor_id) INNER JOIN "
-				  "race_tracker.competition ON (race_tracker.competition.competition_id "
-				  "= race_tracker.competitor_registration.competition_id) WHERE "
-				  "race_tracker.competitor_registration.sensor_id = CAST(x'BEEF' AS int) "
-				  "AND race_tracker.competition.active = True;");*/
-
-
+	return t.prepared("get_active_competitions")(sensor_id).exec();
 }
