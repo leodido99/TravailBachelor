39aa8e859f361f8f45b12a3771d8bace0cc791f9
Léonard Bise <leonard.bise@gmail.com>
2018-08-07 21:12:10 +0200

    Use getter instead of attribute

--- src/RaceGateway/src/race_app_server/race_tracker_data.cpp
+++ src/RaceGateway/src/race_app_server/race_tracker_data.cpp
@@ -36,7 +36,7 @@ int race_tracker_data::insert_data_point(race_mode_record* data_point)
 	std::string insert_query = "INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status)" \
 			 	   "VALUES (1, 1, 0, '2018-05-26 08:30:00-00', ST_MakePoint(46.9933, 6.91612), 130, 160, 5, 9.9, 0);";
 
-	pqxx::result r = this->get_active_competitions(data_point->id);
+	pqxx::result r = this->get_active_competitions(data_point->get_id());
 
 	log(logINFO) << "Query results:";
 	for (auto row: r) {
