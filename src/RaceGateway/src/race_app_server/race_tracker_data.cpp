eeb50ce85829a4c1c78e163abc85ca7a12b05a9c
RaceGateway <leonard.bise@gmail.com>
2018-08-08 20:58:25 +0200

    Fix race_tracker_data DB interface

--- src/RaceGateway/src/race_app_server/race_tracker_data.cpp
+++ src/RaceGateway/src/race_app_server/race_tracker_data.cpp
@@ -15,11 +15,6 @@
 
 #include "logger.h"
 
-pqxx::prepare::invocation& prep_dynamic(unsigned char data, pqxx::prepare::invocation& inv)
-{
-    inv(data);
-    return inv;
-}
 
 race_tracker_data::race_tracker_data(std::string connection_str)
 {
@@ -40,7 +35,7 @@ int race_tracker_data::insert_data_point(race_mode_record* data_point)
 		throw std::runtime_error("Cannot connect to database " + connection_str);
 	}
 
-	/*c.prepare("insert_data_point", "INSERT INTO race_tracker.data_point "
+	c.prepare("insert_data_point", "INSERT INTO race_tracker.data_point "
 		  "(competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status)"
 		  "VALUES ($1, $2, $3, $4, ST_MakePoint($5, $6), $7, $8, $9, $10, $11);");
 
@@ -62,27 +57,19 @@ int race_tracker_data::insert_data_point(race_mode_record* data_point)
 
 	auto row = r[0];
 
-	log(logINFO) << "competitor_id = " << row["competitor_id"].c_str() << "competition_id = " << row["competition_id"].c_str();*/
+	log(logINFO) << "competitor_id = " << row["competitor_id"].c_str() << "competition_id = " << row["competition_id"].c_str();
 
-uint16_t hr = static_cast<uint16_t>(data_point->get_heart_rate());
-uint16_t cad = static_cast<uint16_t>(data_point->get_cadence());
-uint16_t nbsv = static_cast<uint16_t>(data_point->get_nb_sv());
-uint16_t status = static_cast<uint16_t>(data_point->get_status());
-
-	/*t.prepared("insert_data_point")(row["competitor_id"].as<uint16_t>())
+	t.prepared("insert_data_point")(row["competitor_id"].as<uint16_t>())
 				       (row["competition_id"].as<uint16_t>())
 				       (data_point->get_seq())
-				       (0)*/ /* TODO Timestamp */
-				       /*(data_point->get_lat())
+				       (0) /* TODO Timestamp */
+				       (data_point->get_lat())
 				       (data_point->get_lon())
-				       (0)
-				       (0)
-				       (0)
+				       (static_cast<uint16_t>(data_point->get_heart_rate()))
+				       (static_cast<uint16_t>(data_point->get_cadence()))
+				       (static_cast<uint16_t>(data_point->get_nb_sv()))
 				       (data_point->get_hdop())
-				       (0).exec();*/
-
-
-
+				       (static_cast<uint16_t>(data_point->get_status())).exec();
 
 
 
