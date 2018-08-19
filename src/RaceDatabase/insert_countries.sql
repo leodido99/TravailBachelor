27ea50f27f3451562c515b11eb02467e70147c22
Léonard Bise <leonard.bise@gmail.com>
2018-08-19 18:55:53 +0200

    Added France to list of countries

--- src/RaceDatabase/insert_countries.sql
+++ src/RaceDatabase/insert_countries.sql
@@ -1,3 +1,4 @@
 INSERT INTO race_tracker.country VALUES ('CH', 'Suisse', bytea('country/switzerland.svg'));
 INSERT INTO race_tracker.country VALUES ('BG', 'Bulgaria', bytea('country/bulgaria.svg'));
 INSERT INTO race_tracker.country VALUES ('RU', 'Russia', bytea('country/russia.svg'));
+INSERT INTO race_tracker.country VALUES ('FR', 'France', bytea('country/france.svg'));
