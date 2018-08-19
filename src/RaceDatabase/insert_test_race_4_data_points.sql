6997ad7ba8f644e10b1a83333b7ad9eada44cb6e
Léonard Bise <leonard.bise@gmail.com>
2018-08-19 19:22:23 +0200

    Fixed race registration script

--- src/RaceDatabase/insert_test_race_4_data_points.sql
+++ src/RaceDatabase/insert_test_race_4_data_points.sql
@@ -1,19 +1,19 @@
 -- Léonard Bise
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 0, '2018-05-26 08:30:00-00', ST_MakePoint(46.9933, 6.91612), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 0, '2018-05-26 08:30:00-00', ST_MakePoint(46.9933, 6.91612), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 1, '2018-05-26 08:30:00-00', ST_MakePoint(46.993385, 6.916286), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 1, '2018-05-26 08:30:00-00', ST_MakePoint(46.993385, 6.916286), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 2, '2018-05-26 08:30:00-00', ST_MakePoint(46.993795, 6.917177), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 2, '2018-05-26 08:30:00-00', ST_MakePoint(46.993795, 6.917177), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 3, '2018-05-26 08:30:00-00', ST_MakePoint(46.993763, 6.917182), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 3, '2018-05-26 08:30:00-00', ST_MakePoint(46.993763, 6.917182), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 4, '2018-05-26 08:30:00-00', ST_MakePoint(46.994374, 6.918502), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 4, '2018-05-26 08:30:00-00', ST_MakePoint(46.994374, 6.918502), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 5, '2018-05-26 08:30:00-00', ST_MakePoint(46.994932, 6.919414), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 5, '2018-05-26 08:30:00-00', ST_MakePoint(46.994932, 6.919414), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 6, '2018-05-26 08:30:00-00', ST_MakePoint(46.995617, 6.920581), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 6, '2018-05-26 08:30:00-00', ST_MakePoint(46.995617, 6.920581), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
-	VALUES (1, 4, 7, '2018-05-26 08:30:00-00', ST_MakePoint(46.996303, 6.921290), 130, 160, 5, 9.9, 0);
+	VALUES (1, 3, 7, '2018-05-26 08:30:00-00', ST_MakePoint(46.996303, 6.921290), 130, 160, 5, 9.9, 0);
 
 
--- src/RaceDatabase/insert_test_registrations.sql
+++ src/RaceDatabase/insert_test_registrations.sql
@@ -1,12 +1,12 @@
 -- Course Test Planeyse
 INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (1, 1, x'BEEF'::int, 5);
-INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (1, 2, x'BEEF'::int, 13);
+INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (2, 1, x'BEEF'::int, 13);
 -- Fyne Tera 2018
-INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (2, 1, x'BEEF'::int, 102);
+INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (1, 2, x'BEEF'::int, 102);
 INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (2, 2, x'BEEF'::int, 87);
 -- Course Test Valangines
-INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (3, 1, x'BEEF'::int, 44);
-INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (3, 2, x'BEEF'::int, 28);
+INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (1, 3, x'BEEF'::int, 44);
+INSERT INTO race_tracker.competitor_registration (competitor_id, competition_id, sensor_id, bib_number) VALUES (2, 3, x'BEEF'::int, 28);
 
 -- Test race #1
 -- Inactive
