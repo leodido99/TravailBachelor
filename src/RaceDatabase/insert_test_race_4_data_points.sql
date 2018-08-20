c79603672befc71e21a66027432a51bf14b37cb0
Léonard Bise <leonard.bise@gmail.com>
2018-08-20 20:07:49 +0200

    Work on DB queries

--- src/RaceDatabase/insert_test_race_4_data_points.sql
+++ src/RaceDatabase/insert_test_race_4_data_points.sql
@@ -15,5 +15,8 @@ INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, ti
 	VALUES (1, 3, 6, '2018-05-26 08:30:00-00', ST_MakePoint(46.995617, 6.920581), 130, 160, 5, 9.9, 0);
 INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
 	VALUES (1, 3, 7, '2018-05-26 08:30:00-00', ST_MakePoint(46.996303, 6.921290), 130, 160, 5, 9.9, 0);
+INSERT INTO race_tracker.data_point (competitor_id, competition_id, sequence, time_stamp, position, heart_rate, cadence, nb_satellites, position_dop, status) 
+	VALUES (1, 3, 85, '2018-05-26 08:30:00-00', ST_MakePoint(46.992508, 6.914567), 130, 160, 5, 9.9, 0);
+
 
 
