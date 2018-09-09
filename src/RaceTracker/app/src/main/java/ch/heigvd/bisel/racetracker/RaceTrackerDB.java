d5d82d653e454ead6f77871ddb4c6fecb65d25aa
leodido99 <leodido99@gmail.com>
2018-09-09 09:04:14 +0200

    Fix conflict

--- src/RaceTracker/app/src/main/java/ch/heigvd/bisel/racetracker/RaceTrackerDB.java
+++ src/RaceTracker/app/src/main/java/ch/heigvd/bisel/racetracker/RaceTrackerDB.java
@@ -7,7 +7,6 @@ import android.preference.PreferenceManager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
