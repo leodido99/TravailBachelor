eddccdb45ccace0f324cc6715a4b725d76052a2a
Léonard Bise <leonard.bise@gmail.com>
2018-09-15 11:44:44 +0200

    Added Cressier Chaumont race

--- src/RaceDatabase/insert_test_competitions.sql
+++ src/RaceDatabase/insert_test_competitions.sql
@@ -1,6 +1,8 @@
 INSERT INTO race_tracker.competition (name, location, event_date, active, zoom) VALUES ('Course Test Planeyse', ST_MakePoint(46.962279, 6.853059), '2018-08-01 10:00:00-00', True, 17.0);
 INSERT INTO race_tracker.competition (name, location, event_date, active, zoom) VALUES ('Fyne Tera 2018', ST_MakePoint(46.803496, 6.665696), '2018-05-26 08:30:00-00', False, 12.0);
 INSERT INTO race_tracker.competition (name, location, event_date, active, zoom) VALUES ('Course Test Valangines', ST_MakePoint(46.992497, 6.913870), '2018-08-19 14:00:00-00', True, 16.0);
+INSERT INTO race_tracker.competition (competition_id, name, location, event_date, active, zoom) VALUES (6, 'Cressier Chaumont 2018', ST_MakePoint(46.992497, 6.913870), '2018-06-02 10:00:00-00', False, 16.0);
+
 
 -- INSERT INTO race_tracker.competition (name, location, event_date) VALUES ('Test race #1', ST_MakePoint(46.9933, 6.91612), '2018-08-01 10:00:00-00');
 -- INSERT INTO race_tracker.competition (name, location, event_date) VALUES ('Course HEIG-VD', ST_MakePoint(46.781052, 6.646812), '2018-10-15 14:23:55-08');
