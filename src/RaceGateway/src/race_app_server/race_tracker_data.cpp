b459103d8bf26812a7bda01dad4a740714711280
Léonard Bise <leonard.bise@gmail.com>
2018-09-16 14:56:00 +0200

    Check result size to prevent seg fault

--- src/RaceGateway/src/race_app_server/race_tracker_data.cpp
+++ src/RaceGateway/src/race_app_server/race_tracker_data.cpp
@@ -52,17 +52,23 @@ int race_tracker_data::insert_data_point(race_mode_record* data_point)
 
 	pqxx::result r = t.prepared("get_ids")(data_point->get_id()).exec();
 
+	if (r.size() == 0) {
+		log(logWARNING) << "Cannot find competition id";
+		return -1;
+	} else if (r.size() > 1) {
+		log(logWARNING) << r.size() << " competition ids found instead of expected 1";
+	}
+
 	/* Only take the first result, if there are more than one it means the system is badly configured (Several sensors with the same ID) */
 	auto row = r[0];
 
 	log(logINFO) << "competitor_id = " << row["competitor_id"].c_str() << " competition_id = " << row["competition_id"].c_str();
-	log(logINFO) << data_point->to_string();
 
+	/* Create timestamp */
 	std::stringstream mystr;
 	mystr << unsigned(data_point->get_timestamp().get_year()) << "-" << unsigned(data_point->get_timestamp().get_month()) << "-";
 	mystr << unsigned(data_point->get_timestamp().get_day()) << " " << unsigned(data_point->get_timestamp().get_hour()) << ":" << unsigned(data_point->get_timestamp().get_min());
 	mystr << ":" << unsigned(data_point->get_timestamp().get_sec()) << "-00";
-	log(logINFO) << "Timestamp: " << mystr.str();
 
 	t.prepared("insert_data_point")(row["competitor_id"].as<uint16_t>())
 				       (row["competition_id"].as<uint16_t>())
